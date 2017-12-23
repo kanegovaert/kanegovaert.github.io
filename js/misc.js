16c11e3fc447cf3e1980d44a5699d700deb060be
Kane <kg01@protonmail.com>
2017-12-23 13:06:13 -0500

    New website of Kane Govaert

---
+++ js/misc.js
@@ -0,0 +1,17 @@
+
+var waypoint = new Waypoint({
+  element: document.getElementById('about-us-text-one'),
+  handler: function() {
+    notify('Basic waypoint triggered')
+  }
+})
+
+
+
+/*
+
+$("#about-us-text-one").animate({
+          fontSize: "10%"
+      }, 1000);
+
+      */
---
+++ js/scroll-to.js
@@ -0,0 +1,38 @@
+function scrollTo(element, time) {
+  $('html, body').animate({
+      scrollTop: $(element).offset().top
+  }, time);
+}
+
+
+$("#flat-button-one").live('click', function() {
+    scrollTo("#header-two", 2000);
+    setTimeout(200);
+    scrollTo("#first-about-us-item", 1500);
+    setTimeout(200);
+    scrollTo("#second-about-us-item", 1500);
+    setTimeout(200);
+    scrollTo("#last-about-us-item", 1500);
+});
+
+
+function appendText() {
+    var exittext = "<p class='exit-text'>Thanks for visiting</p>";
+    $("body").append(exittext);
+    $(".exit-text").css({"textAlign" : "center"});
+}
+$("#flat-button-two").live('click', function() {
+    scrollTo(".header-three", 2000);
+});
+
+$("#flat-button-three").live('click', function() {
+    scrollTo(".header-four", 2000);
+});
+
+$("#flat-button-four").live('click', function() {
+    scrollTo(".header-five", 2000);
+});
+
+$(".down-arrow").live('click', function() {
+    scrollTo(".header-two", 2000)
+});
